e93f61afc37a259f3b239126d82036ef62f780da
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2021-09-12 22:30:44 +0800

    Get game together.

--- Assets/Scripts/MoveToScene.cs
+++ Assets/Scripts/MoveToScene.cs
@@ -1,8 +1,24 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MoveToScene : MonoBehaviour {
+    // For Unity, when you get out of scene and go to 
+    // another scene, the prev scene will stop existing 
+    // in memory. 
+    private void start() {
+        // This is good for conveying message to other parts of the game. 
+        // For if you don't want to destroy the scene... 
+        // gameObject is refering to the game object this script is attached to.
+        DontDestroyOnLoad(gameObject);  
+    }
+
     public void changeScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame() {
+        Debug.Log("You quit the game!");
+        Application.Quit();
     }
 } 
--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Key Notes
 1) Do not make any changes in play mode since Unity will not save anything! 
 2) You must add the scene you want to in build settings. 
 3) In the build setting, the topmost one is the scene you will start from.
+4) Prefabs are game object that you can use in multiple scenes.
+5) Event system controls the UI stuff like buttons... Must have for all your scenes.
